7e4b7c70c89efa10985b55203ccf7d6e2ac1e35d
Emmanuel Bustos <ema2159@gmail.com>
2023-02-19 21:48:13 +0200

    fix: Temporarily remove transactions model. Only leave Users model

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from .database import Base
 
@@ -13,15 +13,15 @@ class User(Base):
     address = Column(String)
     phone_num = Column(String)
 
-    transactions = relationship("Transaction", back_populates=True)
+    # transactions = relationship("Transaction", back_populates=True)
 
 
-class Transaction(Base):
-    __tablename__ = "transaction"
+# class Transaction(Base):
+#     __tablename__ = "transaction"
 
-    amount = Column(Integer)
-    id = Column(Integer, primary_key=True, index=True)
-    user1_id = Column(Integer, ForeignKey("user.id"))
-    user2_id = Column(Integer, ForeignKey("user.id"))
+#     amount = Column(Integer)
+#     id = Column(Integer, primary_key=True, index=True)
+#     user1_id = Column(Integer, ForeignKey("user.id"))
+#     user2_id = Column(Integer, ForeignKey("user.id"))
 
-    user = relationship("User", back_populates=True)
+#     user = relationship("User", back_populates=True)
