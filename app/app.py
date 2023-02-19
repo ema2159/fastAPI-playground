1135b954773c5808e07ce3f30ff955e05fd4e3d0
Emmanuel Bustos <ema2159@gmail.com>
2023-02-19 21:48:59 +0200

    feat: Add API CRUD functions

--- app/app.py
+++ app/app.py
@@ -1,16 +1,41 @@
-from fastapi import FastAPI
-from sql_app.schemas import UserBase
+from fastapi import Depends, FastAPI, HTTPException
+from sql_app import crud, models, schemas
+from sql_app.database import SessionLocal, engine
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/", tags=["ROOT"])
-async def root():
-    return {"Hello": "World"}
+
+@app.post("/users", tags=["userAPI"])
+def post_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
 
-@app.post("/user", tags=["userAPI"])
-async def post_user(user: UserBase):
-    print(user)
-    return "Success"
+@app.get("/users", tags=["userAPI"])
+def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_users(db, skip, limit)
+
+
+@app.get("/users/{user_id}", tags=["userAPI"])
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    return crud.get_user(db, user_id)
+
+
+@app.delete("/users/{user_id}", tags=["userAPI"])
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    if not crud.delete_user(db, user_id):
+        raise HTTPException(status_code=404, detail="Could not delete. User not found")
+    return {"ok": True}
